932bd723711c724bc93d1e6a466f523552b586fd
BlackhatVampire <vishujan28@gmail.com>
2026-09-10 21:21:12 +0530

    Time: 53 ms (75.86%), Space: 24 MB (55.59%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,44 +8,50 @@
  */
 class Solution {
 public:
-    ListNode* moveksteps(ListNode *head , int k){
+    ListNode* moveksteps(ListNode* head, int k) {
         ListNode* temp = head;
-        while(k--){
+        while (k--) {
             temp = temp->next;
         }
         return temp;
     }
-    int checklength(ListNode *head){
+    int checklength(ListNode* head) {
         int n = 0;
         ListNode* temp = head;
-        while(temp){
+        while (temp) {
             temp = temp->next;
             n++;
         }
         return n;
     }
 
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* list1 = headA;
-        ListNode* list2 = headB;
-        int l1 = checklength(list1);
-        int l2 = checklength(list2);
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+
+        int l1 = checklength(headA);
+        int l2 = checklength(headB);
         int k = 0;
-        ListNode* ptr1 = list1;
-        ListNode* ptr2 = list2;
-        if(l1>l2){
-            k = l1-l2;  
-            ptr1 = moveksteps(list1,k);
+        ListNode* ptr1 = headA;
+        ListNode* ptr2 = headB;
+        if (l1 > l2) {
+            k = l1 - l2;
+            ptr1 = moveksteps(ptr1, k);
+        } else {
+            k = l2 - l1;
+            ptr2 = moveksteps(ptr2, k);
         }
-        else{
-            k=l2-l1;
-            ptr2 = moveksteps(list2,k);
-        }
-        while(ptr1 && ptr2){
-            if(ptr1==ptr2) return ptr1;
+        while (ptr1 && ptr2) {
+            if (ptr1 == ptr2)
+                return ptr1;
             ptr1 = ptr1->next;
             ptr2 = ptr2->next;
         }
         return NULL;
+        /*ptr1->val == ptr2->val
+
+        Because values can be identical in completely different nodes:
+        List A: 1 → 7 → 9
+        List B: 4 → 7 → 9
+
+        */
     }
 };
